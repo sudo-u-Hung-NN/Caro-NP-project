cd6afd7ebbf8e95d3e7b1d71f834baa239d34032
sudo-u-Hung-NN <hung.nn184118@sis.hust.edu.vn>
2022-01-06 21:06:53 +0700

    Final submission

--- src/Server_side/Functions/cancel.c
+++ src/Server_side/Functions/cancel.c
@@ -3,35 +3,52 @@
 extern NodeUser *root;
 extern NodeGame *game_root;
 
+extern thread_local Player *myself;
+extern thread_local Player *rival;
+
 
 void process_cancel(message *msg, User* current_user) {
     size_t rep_len = sizeof(reply);
     
-    // get game id
-    int game_id = atoi(getData(msg));
-
-    // get game by id
-    NodeGame *nodegame = search_NodeGame_byId(game_root, game_id);
-
-    if (nodegame == NULL) {
+    if (myself == NULL) {
+        WARNING("Trying to excecute cancel while no game was initiated");
         send(current_user->listener, create_reply(ko, "NULL_CANCEL"), rep_len, 0);
-        return;
-    } else if (nodegame->game->player1->user != current_user) {
-        send(current_user->listener, create_reply(ko, "INVALID_CANCEL"), rep_len, 0);
-        return;
+
+    } else {
+        Game *current_game = myself->current_game;
+
+        if (current_game == NULL) {
+            send(current_user->listener, create_reply(ko, "NULL_CANCEL"), rep_len, 0);
+            return;
+        } else if (current_game->player1->user != current_user) {
+            send(current_user->listener, create_reply(ko, "INVALID_CANCEL"), rep_len, 0);
+            return;
+        }
+
+        INFORLOG("Closing the game");
+        // Free players
+        INFORLOG("Free myself");
+        free(myself);
+        INFORLOG("Free rival");
+        free(rival);
+
+        // Free spectators
+        INFORLOG("Free spectator");
+        free_Spectator_List(current_game);
+
+        // Set game to closed
+        NodeGame *game = search_NodeGame_byId(game_root, current_game->id);
+        if (game == NULL) {
+            WARNING("Trying to close a null game");
+        } else {
+            game->playing = -1;
+        }       
+        INFORLOG("Game closed");
+
+        // send cancel to player 2
+        send(current_game->player2->user->listener, create_reply(cancel, "CANCELED"), rep_len, 0);
+
+        // send notification to myself
+        send(current_user->listener, create_reply(ok, "You have cancel successfully"), rep_len, 0);
     }
-
-    // Destroy the game
-    free(nodegame->game->player1);
-    free(nodegame->game->player2);
-    free(nodegame->game);
-    close_NodeGame_byId(game_root, game_id);
-
-    // send cancel to player 2
-    send(nodegame->game->player2->user->listener, create_reply(cancel, "CANCELED"), rep_len, 0);
-
-    // send notification to myself
-    send(current_user->listener, create_reply(ok, "You have cancel successfully"), rep_len, 0);
-
-    // send(current_user->listener, create_reply(ko, "ON_DEVELOP"), sizeof(reply), 0);
 }
